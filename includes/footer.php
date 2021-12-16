a61ead60e9cf4b07ec6cf5d0a28304997738c278
Jyabka <sokolovkita@gmail.com>
2021-12-16 11:18:21 +0700

    Register & Auth system

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<footer class="footer mt-5">
+  <div class="container">
+    <hr>
+    <div class="row">
+      <div class="col-md-6">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-12 mt-3">
+      <div class="alert alert-secondary text-right" role="alert">
+        <h4 class="alert-heading"></h4>
+      </div>
+    </div>
+  </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+<script>
+  $(document).ready(function() {
+    $('body').bootstrapMaterialDesign();
+  });
+</script>
